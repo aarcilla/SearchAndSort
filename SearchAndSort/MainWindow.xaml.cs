d65017c6516077ebd5c6142760e0ff6d19cd8982
Alistair Arcilla <alistair.arcilla@outlook.com>
2015-04-14 12:52:12 +1000

    Implemented event handler code for linear search, including button and associated input text boxes

--- SearchAndSort/MainWindow.xaml.cs
+++ SearchAndSort/MainWindow.xaml.cs
@@ -71,5 +71,86 @@ namespace SearchAndSort
                 control.IsEnabled = false;
             }
         }
+
+        private void linearButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputBox.Text)
+                || string.IsNullOrWhiteSpace(desiredNumBox.Text))
+                return;
+
+            int[] nums = ParseSpaceDelimitedIntegers(inputBox.Text);
+
+            int desiredNum;
+            bool tryParseDesiredNum = Int32.TryParse(desiredNumBox.Text, out desiredNum);
+
+            if (tryParseDesiredNum)
+            {
+                Search search = new Search();
+                int? result = search.Linear(nums, desiredNum);
+
+                if (result.HasValue)
+                {
+                    outputLabel.Content = string.Format("Found {0} at index {1}", desiredNum, result.Value);
+                }
+                else
+                {
+                    outputLabel.Content = "Desired number not found.";
+                }
+            }
+            else
+            {
+                outputLabel.Content = "Specified desired number is not an integer.";
+            }
+        }
+
+        private void binaryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region Helper methods
+
+        private int[] ParseSpaceDelimitedIntegers(string numsString)
+        {
+            List<int> nums = new List<int>();
+
+            var addToNumsList = new Action<string>((numAsString) =>
+            {
+                int num;
+                bool tryParseNum = Int32.TryParse(numAsString, out num);
+
+                if (tryParseNum)
+                    nums.Add(num);
+            });
+
+            string currentNumAsString = string.Empty;
+            int currentIndex = 0;
+            while(currentIndex < numsString.Length) 
+            {
+                if (numsString.Substring(currentIndex, 1) == " ")
+                {
+                    addToNumsList(currentNumAsString);
+
+                    currentNumAsString = string.Empty;
+
+                }
+                else
+                {
+                    currentNumAsString += numsString.Substring(currentIndex, 1);
+                }
+
+                currentIndex++;
+            }
+
+            // Add last number in space-delimited string
+            if (!string.IsNullOrWhiteSpace(currentNumAsString))
+            {
+                addToNumsList(currentNumAsString);
+            }
+
+            return nums.ToArray();
+        }
+
+        #endregion
     }
 }
